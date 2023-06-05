e3ab82d9bee76cd7f1770c281e65599e40b7d2a3
smajaas <smajaas@gmail.com>
2023-06-05 21:06:54 +0530

    single product loading error

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,6 +15,33 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const SingleProductPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext();
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`);
+  }, [id]);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  }, [error]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
   return <h4>single product page</h4>;
 };
 
