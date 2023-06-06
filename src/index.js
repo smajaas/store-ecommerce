7482ac0586f8d60877792374bd50dd36f7d44085
smajaas <smajaas@gmail.com>
2023-06-06 11:32:06 +0530

    single product - images

--- src/index.js
+++ src/index.js
@@ -12,9 +12,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <ProductsProvider>
-      <App />
-    </ProductsProvider>
-  </React.StrictMode>
+  <ProductsProvider>
+    <App />
+  </ProductsProvider>
 );
