4841cb265df837557f3d723f16f5497128cd0095
smajaas <smajaas@gmail.com>
2023-06-07 18:01:45 +0530

    fiter companies

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -64,6 +64,25 @@ const Filters = () => {
             </div>
           </div>
           {/* end of category */}
+          {/* company */}
+          <div className='form-control'>
+            <h5>company</h5>
+            <select
+              name='company'
+              value={company}
+              onChange={updateFilters}
+              className='company'
+            >
+              {companies.map((c, index) => {
+                return (
+                  <option key={index} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* end of company */}
         </form>
       </div>
     </Wrapper>
