cf0e910695c1cfeb4dc96b1bac490c7e91609d12
smajaas <smajaas@gmail.com>
2023-06-08 22:52:26 +0530

    cart page setup

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -11,6 +11,17 @@ const cart_reducer = (state, action) => {
     const { id, color, amount, product } = action.payload;
     const tempItem = state.cart.find((i) => i.id === id + color);
     if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount;
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max;
+          }
+          return { ...cartItem, amount: newAmount };
+        } else {
+          return cartItem;
+        }
+      });
     } else {
       const newItem = {
         id: id + color,
@@ -24,7 +35,7 @@ const cart_reducer = (state, action) => {
       return { ...state, cart: [...state.cart, newItem] };
     }
   }
-  return state;
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
