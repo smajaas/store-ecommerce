4a884d8d994d2f2dc1a49f71ac0b4c4b872b899b
smajaas <smajaas@gmail.com>
2023-06-09 12:13:58 +0530

    cart toggle amount

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -42,7 +42,10 @@ export const CartProvider = ({ children }) => {
   };
 
   //toggle amount
-  const toggleAmount = (id, value) => {};
+  const toggleAmount = (id, value) => {
+    console.log(id, value);
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, value } });
+  };
   //clear cart
   const clearCart = () => {
     dispatch({ type: CLEAR_CART });
